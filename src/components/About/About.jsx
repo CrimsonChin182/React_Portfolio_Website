629d06588b20ebe72baf32a3587dc82cb14f0bec
Alex <towey039@d.umn.edu>
2025-03-11 13:10:20 -0500

    Princess in Command Crafts changes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -12,7 +12,7 @@ export const About = () => {
                         
                         <div className={styles.aboutItemText}>
                             <h3>Frontend Developer</h3>
-                            <p>I'm a Frontend Developer with experience in building responsive and user-friendly websites.</p>
+                            <p>I'm a Frontend Developer with experience in building responsive and user-friendly websites</p>
                         </div>
                     </li>
                     <li className={styles.aboutItem}>
@@ -26,28 +26,28 @@ export const About = () => {
                         
                         <div className={styles.aboutItemText}>
                             <h3>Cybersecurity</h3>
-                            <p>I have experience developing code to prevent SQL injection attacks, ensuring database security and integrity.</p>
+                            <p>I have experience developing code to prevent SQL injection attacks, ensuring database security and integrity</p>
                         </div>
                     </li>
                     <li className={styles.aboutItem}>
                         
                         <div className={styles.aboutItemText}>
                             <h3>UX designer</h3>
-                            <p>Through the Minnetonka High School VANTAGE program, I interviewed clients and developed responsive wireframes based on their needs.</p>
+                            <p>Through the Minnetonka High School VANTAGE program, I interviewed clients and developed responsive wireframes based on their needs</p>
                         </div>
                     </li>
                     <li className={styles.aboutItem}>
                         
                         <div className={styles.aboutItemText}>
                             <h3>Group Collaboration</h3>
-                            <p>I have worked collaboratively with teams on multiple projects, contributing to code development, creating test cases, and coordinating efforts to upload and merge shared progress using GitHub.</p>
+                            <p>I have worked collaboratively with teams on multiple projects, contributing to code development, creating test cases, and coordinating efforts to upload and merge shared progress using GitHub</p>
                         </div>
                     </li>
                     <li className={styles.aboutItem}>
                         
                         <div className={styles.aboutItemText}>
                             <h3>Custom PC Expertise</h3>
-                            <p>I have built, optimized, repaired, and resold custom gaming PCs for personal use, as well as for friends and clients.</p>
+                            <p>I have built, optimized, repaired, and resold custom gaming PCs for personal use, as well as for friends and clients</p>
                         </div>
                     </li>
                 </ul>
